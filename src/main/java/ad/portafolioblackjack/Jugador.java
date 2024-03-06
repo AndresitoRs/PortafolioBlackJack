912c3969572a21d5de79c09904d1ecf63547aca5
andres <andres.rivada@iessanmamede.com>
2024-03-06 15:15:06 +0100

    Creación de las clases carta y Jugador, se procede a hacer los métodos e implementar la lógica en el controlador

---
+++ src/main/java/ad/portafolioblackjack/Jugador.java
@@ -0,0 +1,24 @@
+package ad.portafolioblackjack;
+
+public class Jugador {
+
+    private String nombre;
+    private int puntuación = 0;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntuación() {
+        return puntuación;
+    }
+
+    public void setPuntuación(int puntuación) {
+        this.puntuación = puntuación;
+    }
+
+}
